31c66493954fe421dcead1163b1dcc7778a416e7
94timoha <94timoha@gmail.com>
2021-12-08 12:38:00 +0300

    after 12 lessons

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return         <header className={'header'}>
+            <img
+                src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiHbk4QxhadVP1LFN0r_9JWQgx0AFhrlt8bg&usqp=CAU'} alt ={'ss'}/>
+        </header>;
+    }
+ export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Profile = () => {
+    return         <div className={'content'}>
+        <div>
+            <img
+                src={'https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'} alt ={'ss'}/>
+        </div>
+        <div>
+            <img
+                src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS27oZW3GPaCyGG172TNgXUjfyJxSq-ETp2AA&usqp=CAU'} alt ={'ss'}/>
+        </div>
+        Main Content
+    </div>
+    }
+ export default Profile;
